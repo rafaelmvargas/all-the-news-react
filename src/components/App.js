782cb8f315adecacaeada765b09ed7807674fdfc
Rafael Vargas <rafael.vargas@outlook.com>
2021-11-02 21:27:30 -0400

    hw start

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import Header from "./Header";
+import Nav from "./Nav";
+import Stories from "./Stories";
+
+const navItems = ["arts", "books", "fashion", "food", "movies", "travel"];
+const nytapi = "b0EDOD1ajEoIcL1Iv8vOHV7oS07SQZmh";
+const section = "arts";
 
 function App() {
+  const [stories, setStories] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    fetch(
+      `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`
+    )
+      .then((response) => response.json())
+      .then((data) => setStories(data.results));
+  }, []);
+
   return (
-    <div>
-      <h4>Let's go!</h4>
-    </div>
+    <>
+      <Header siteTitle="All the news that Fits We Print" />
+      <Nav navItems={navItems} />
+      <Stories stories={stories}/>
+    </>
   );
 }
 
