b1b9293e2fbb17c12193e507aa0a1011458f268e
Rafael Vargas <rafael.vargas@outlook.com>
2021-11-04 16:23:41 -0400

    loading

--- src/components/App.js
+++ src/components/App.js
@@ -12,18 +12,24 @@ function App() {
   const [loading, setLoading] = React.useState(false);
 
   React.useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`
     )
       .then((response) => response.json())
-      .then((data) => setStories(data.results));
+      .then((data) => setStories(data.results))
+      .then(setLoading(false));
   }, []);
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
 
   return (
     <>
       <Header siteTitle="All the news that Fits We Print" />
       <Nav navItems={navItems} />
-      <Stories stories={stories}/>
+      <Stories stories={stories} />
     </>
   );
 }
